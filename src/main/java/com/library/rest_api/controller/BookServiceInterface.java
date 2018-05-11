1030f28072a48fca6f6a724bff687d1d6bbd854d
JarekSoja <jarek.soja@gmail.com>
2018-05-11 20:17:50 +0200

    REST controllers in the making

--- src/main/java/com/library/rest_api/controller/BookServiceInterface.java
+++ src/main/java/com/library/rest_api/controller/BookServiceInterface.java
@@ -1,6 +1,8 @@
 package com.library.rest_api.controller;
 
+import com.library.rest_api.domain.BookCopyDto;
 import com.library.rest_api.domain.BookTitleDto;
+import com.library.rest_api.domain.NewBookCopy;
 import com.library.rest_api.domain.NewBookTitle;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,4 +28,26 @@ public interface BookServiceInterface {
 
     @RequestMapping(method = RequestMethod.PUT, value = "updateBookTitle")
     BookTitleDto updateBookTitle (@RequestBody BookTitleDto bookTitleDto);
+
+    @RequestMapping(method = RequestMethod.POST, value = "newBookCopy")
+    NewBookCopy newBookCopy(@RequestBody BookCopyDto bookCopyDto);
+
+    @RequestMapping(method = RequestMethod.GET, value = "getAllBookCopies")
+    List<BookCopyDto> getAllBookCopies();
+
+    @RequestMapping(method = RequestMethod.GET, value = "getBookCopy")
+    BookCopyDto getBookCopy(@RequestParam Long bookCopyId);
+
+    @RequestMapping(method = RequestMethod.GET, value = "getBookCopiesByAuthor")
+    List<BookCopyDto> getBookCopiesByAuthor(@RequestParam String author);
+
+    @RequestMapping(method = RequestMethod.GET, value = "getBookCopiesByTitle")
+    List<BookCopyDto> getBookCopiesByTitle(@RequestParam String title);
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "removeBookCopy")
+    void removeBookCopy(@RequestParam Long bookCopyId);
+
+    @RequestMapping(method = RequestMethod.PUT, value = "updateBookCopy")
+    BookCopyDto updateBooCopy (@RequestBody BookCopyDto bookCopyDto);
+
 }
